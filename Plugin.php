14bba950dbad982f4525e37ceec88c1bd73c718a
vndroid <diracsvip@gmail.com>
2022-04-06 15:35:25 +0800

    Update
    - 整理代码格式
    - 增加新功能过滤中文评论

--- Plugin.php
+++ Plugin.php
@@ -1,10 +1,11 @@
 <?php
+
 /**
  * 评论通知推送至 IFTTT Webhooks
  *
  * @package Comment2IFTTT
  * @author 神代綺凜
- * @version 1.0.0
+ * @version 2.0.0
  * @link https://moe.best
  */
 class Comment2IFTTT_Plugin implements Typecho_Plugin_Interface
@@ -16,7 +17,8 @@ class Comment2IFTTT_Plugin implements Typecho_Plugin_Interface
      * @return void
      * @throws Typecho_Plugin_Exception
      */
-    public static function activate() {
+    public static function activate()
+    {
         Typecho_Plugin::factory('Widget_Feedback')->comment = array('Comment2IFTTT_Plugin', 'msgPush');
         return _t('请记得进入插件配置 IFTTT Webhooks key');
     }
@@ -29,7 +31,9 @@ class Comment2IFTTT_Plugin implements Typecho_Plugin_Interface
      * @return void
      * @throws Typecho_Plugin_Exception
      */
-    public static function deactivate() {}
+    public static function deactivate()
+    {
+    }
 
     /**
      * 获取插件配置面板
@@ -38,7 +42,8 @@ class Comment2IFTTT_Plugin implements Typecho_Plugin_Interface
      * @param Typecho_Widget_Helper_Form $form 配置面板
      * @return void
      */
-    public static function config(Typecho_Widget_Helper_Form $form) {
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {
         $key = new Typecho_Widget_Helper_Form_Element_Text('whKey', NULL, NULL, _t('Webhooks Key'), _t('想要获取 Webhooks key 则需要启用 <a href="https://ifttt.com/maker_webhooks" target="_blank">IFTTT 的 Webhooks 服务</a>，然后点击右上角的“Documentation”来查看'));
         $form->addInput($key->addRule('required', _t('您必须填写 Webhooks key')));
 
@@ -49,8 +54,14 @@ class Comment2IFTTT_Plugin implements Typecho_Plugin_Interface
             array(
                 '1' => '是',
                 '0' => '否'
-            ),'1', _t('当评论者为博主时不推送'), _t('启用后，若评论者为博主，则不会推送至 IFTTT Webhooks'));
+            ), '1', _t('当评论者为博主时不推送'), _t('启用后，若评论者为博主，则不会推送至 IFTTT Webhooks'));
         $form->addInput($excludeBlogger);
+        $filterNoneChinese = new Typecho_Widget_Helper_Form_Element_Radio('filterNoneChinese',
+            array(
+                '1' => '是',
+                '0' => '否'
+            ), '1', _t('是否过滤非中文评论'), _t('启用后，若评论中不含中文字符，则不会推送至 IFTTT Webhooks'));
+        $form->addInput($filterNoneChinese);
     }
 
     /**
@@ -60,7 +71,9 @@ class Comment2IFTTT_Plugin implements Typecho_Plugin_Interface
      * @param Typecho_Widget_Helper_Form $form
      * @return void
      */
-    public static function personalConfig(Typecho_Widget_Helper_Form $form) {}
+    public static function personalConfig(Typecho_Widget_Helper_Form $form)
+    {
+    }
 
     /**
      * 推送至 IFTTT Webhooks
@@ -68,9 +81,10 @@ class Comment2IFTTT_Plugin implements Typecho_Plugin_Interface
      * @access public
      * @param array $comment 评论结构
      * @param Typecho_Widget $post 被评论的文章
-     * @return $comment
+     * @return array $comment
      */
-    public static function msgPush($comment, $post) {
+    public static function msgPush($comment, $post)
+    {
         $options = Typecho_Widget::widget('Widget_Options')->plugin('Comment2IFTTT');
 
         $whKey = $options->whKey;
@@ -84,7 +98,7 @@ class Comment2IFTTT_Plugin implements Typecho_Plugin_Interface
         $headers = array(
             "Content-type: application/json"
         );
-        $url = 'https://maker.ifttt.com/trigger/'.$evName.'/with/key/'.$whKey;
+        $url = 'https://maker.ifttt.com/trigger/' . $evName . '/with/key/' . $whKey;
         $data = array(
             'value1' => $post->title,
             'value2' => $comment['author'],
@@ -101,6 +115,9 @@ class Comment2IFTTT_Plugin implements Typecho_Plugin_Interface
         curl_exec($ch);
         curl_close($ch);
 
-        return $comment;
+        $charCHN = preg_match('/[\x{4e00}-\x{9fa5}]/u', $comment['text']);
+        if ($charCHN == 1) {
+            return $comment;
+        }
     }
 }
